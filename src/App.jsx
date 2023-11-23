f1f16c10580e03e70624d641805a438434b69b99
@wisofer 17 <87929107+Wisofer@users.noreply.github.com>
2023-11-23 10:32:24 -0600

    gregando nuevas configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import io from 'socket.io-client'
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
-
-
-const socket = io("/")
+const socket = io('https://servidor-lake.vercel.app');
 
 const App = () => {
+  useEffect(() => {
+    // Manejo de eventos y lógica de los sockets
+    socket.on('connect', () => {
+      console.log('Conectado al servidor');
+      
+      // Lógica adicional al establecer la conexión
+    });
 
-  console.log("La conexion fue exitosa :D",socket)
+    socket.on('disconnect', () => {
+      console.log('Desconectado del servidor');
+      
+      // Lógica adicional al desconectarse
+    });
+
+    // Suscribirse a eventos o enviar mensajes al servidor
+    socket.emit('mensaje', 'Hola servidor', (respuesta) => {
+      console.log('Respuesta del servidor:', respuesta);
+    });
+
+    // Limpiar los eventos al desmontar el componente
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div>
-        <p>Hola amigos :D</p>
+      <p>Hola amigos :D</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/socket.io/socket.io.js': {
+      '/socket.io': {
         target:'https://servidor-lake.vercel.app',
         ws: true
       }
